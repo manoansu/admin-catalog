bb3d3c331a214200b07aa6e4409dc2bb937688b9
Ansumane Mané <manoansu@gmail.com>
2025-03-15 08:48:24 +0000

    First commit

---
+++ admin-catalog/src/test/java/pt/amane/admin_catalog/AdminCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.amane.admin_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
